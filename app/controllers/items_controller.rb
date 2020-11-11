7360c4fd29da635ff350413f5570bbee66121d80
nxu4559 <sevenocean7731@gmail.com>
2020-11-11 10:39:06 +0900

    ユーザー管理機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-   
+    @user = User.all
   end
 
- 
+  def move_to_index
+    redirect_to action: index unless user_signed_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create]
+  get "user/:id" => "users#show"
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+  resources :items do
   end
-
+end
