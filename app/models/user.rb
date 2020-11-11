254bc1b32b69d95263f1e52abc2c0717f73a571d
nxu4559 <sevenocean7731@gmail.com>
2020-11-11 15:47:08 +0900

    修正2
    名前とふりがなが必須であることを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,16 @@ class User < ApplicationRecord
   # パスワードが半角英字と半角数字それぞれ１文字以上を含む8文字以上100文字以下の文字列
   PASSWORD_RULE = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_RULE, message: 'には英字と数字の両方を含めて入力してください'
+  # 名前は必須
+  validates :last_name, presence: true
+  validates :first_name, presence: true
   # 名前は全角文字で入力されているか
   NAME_RULE = /\A[ぁ-んァ-ヶー一-龠]+\z/.freeze
   validates_format_of :last_name, with: NAME_RULE, message: 'は全角文字で設定してください'
   validates_format_of :first_name, with: NAME_RULE, message: 'は全角文字で設定してください'
+  # ふりがなは名前それぞれで必須であること
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
   # ふりがなは全角カタカナで入力されているか
   FURIGANA_NAME_RULE = /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/.freeze
   validates_format_of :last_name_kana, with: FURIGANA_NAME_RULE, message: 'は全角カタカナで設定してください'
