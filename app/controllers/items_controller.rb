d98fe16bd746eb0310967f748a18afbb9f6f8ad8
nxu4559 <sevenocean7731@gmail.com>
2020-11-18 23:20:47 +0900

    商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,29 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :authenticate_user!, only: [:new]
 
   def index
+    @items = Item.order('created_at DESC')
   end
 
-  def move_to_index
-    redirect_to root_path if current_user.id == @item.user_id
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
   end
 
   private
+
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:price, :image, :name, :description, :category_id, :condition_id, :postage_prayer_id, :prefecture_id, :days_id).merge(user_id: current_user.id)
+  end
 end
