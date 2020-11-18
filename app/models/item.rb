348dba79c5ffd4df87e5b59485526f3f7b297a10
nxu4559 <sevenocean7731@gmail.com>
2020-11-18 23:19:58 +0900

    Active Hash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,32 @@
 class Item < ApplicationRecord
-  has_many :comments
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :postage
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :category
-  belongs_to_active_hash :days
-  has_one :purchase
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :postage_prayer_id
+    validates :prefecture_id
+    validates :days_id
+    validates :price, format: { with: /\A\d+\z/ },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage_prayer
+  belongs_to :prefecture
+  belongs_to :day
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_prayer_id
+    validates :prefecture_id
+    validates :days_id
+  end
 end
